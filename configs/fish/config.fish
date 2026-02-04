005c68ebde768061c7f5dab328f704927672490a
TemaSoul <stepanov93555@gmail.com>
2026-02-04 15:34:56 +0300

    update config

---
+++ configs/fish/config.fish
@@ -0,0 +1,5 @@
+cat ~/.cache/wal/sequences
+
+alias sudo="doas"
+alias reflector-update="doas reflector --country "RU,GE,BY,AZ" --latest 10 --sort rate --fastest 5 --save /etc/pacman.d/mirrorlist"
+alias zlj="zellij"
